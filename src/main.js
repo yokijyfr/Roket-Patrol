b8a9f11601f6dc30851718449701f8cdda2ca0dd
yokijyfr <103284162+yokijyfr@users.noreply.github.com>
2022-04-10 16:49:19 -0700

    moving rocket

--- src/main.js
+++ src/main.js
@@ -8,3 +8,5 @@ let game = new Phaser.Game(config);
 // set UI sizes
 let borderUISize = game.config.height / 15;
 let borderPadding = borderUISize / 3;
+
+let keyF, keyR, keyLEFT, keyRIGHT;
--- src/scenes/Menu.js
+++ src/scenes/Menu.js
@@ -8,3 +8,4 @@ class Menu extends Phaser.Scene {
         this.scene.start("playScene");
     }
 }
+
