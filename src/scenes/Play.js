205c1f9818312af4b6f0ffe925494dcad0540319
yokijyfr <103284162+yokijyfr@users.noreply.github.com>
2022-04-10 17:23:33 -0700

    added points

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -37,6 +37,24 @@ class Play extends Phaser.Scene {
             frames: this.anims.generateFrameNumbers('explosion', { start: 0, end: 9, first: 0}),
             frameRate: 30
         });
+
+        this.p1Score = 0;
+
+        let scoreConfig = {
+            fontFamily: 'Courier',
+            fontSize: '28px',
+            backgroundColor: '#F3B141',
+            color: '#843605',
+            align: 'right',
+            padding: {
+              top: 5,
+              bottom: 5,
+            },
+            fixedWidth: 100
+        }
+        this.scoreLeft = this.add.text(borderUISize + borderPadding, borderUISize + borderPadding*2, this.p1Score, scoreConfig);
+
+
     }
 
     update() {
@@ -81,7 +99,10 @@ class Play extends Phaser.Scene {
           ship.reset();                         // reset ship position
           ship.alpha = 1;                       // make ship visible again
           boom.destroy();                       // remove explosion sprite
-        });       
+        });
+
+        this.p1Score += ship.points;
+        this.scoreLeft.text = this.p1Score;
     }
 
 }
